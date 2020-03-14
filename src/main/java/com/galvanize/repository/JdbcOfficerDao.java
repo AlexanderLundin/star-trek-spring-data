578f4fa0ef425187e4108f6fbb3919711767acf0
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-14 12:15:06 -0700

    findAllOfficers
    used row mapper and final strings to clean up query

--- src/main/java/com/galvanize/repository/JdbcOfficerDao.java
+++ src/main/java/com/galvanize/repository/JdbcOfficerDao.java
@@ -4,14 +4,19 @@ import com.galvanize.entities.Officer;
 import com.galvanize.entities.Rank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class JdbcOfficerDao {
 
     private JdbcTemplate jdbcTemplate;
     private final String COUNT_OFFICERS = "select count(*) from officers";
-
+    private final String FETCH_ALL_OFFICERS = "select id, officer_rank, first_name, last_name from officers";
 
     public JdbcOfficerDao(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
@@ -20,4 +25,22 @@ public class JdbcOfficerDao {
     public int countOfficers() {
         return jdbcTemplate.queryForObject(COUNT_OFFICERS, Integer.class);
     }
+
+    public List<Officer> findAllOfficers() {
+        return jdbcTemplate.query(FETCH_ALL_OFFICERS, new OfficerMapper());
+    }
+}
+
+class OfficerMapper implements RowMapper {
+
+    @Override
+    public Officer mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Officer officer = new Officer();
+        officer.setId((long) rs.getInt("id"));
+        officer.setRank(Rank.valueOf(rs.getString("officer_rank")));
+        officer.setFirst(rs.getString("first_name"));
+        officer.setLast(rs.getString("last_name"));
+        return officer;
+    }
+
 }
--- src/test/java/com/galvanize/repository/JdbcOfficerDaoTest.java
+++ src/test/java/com/galvanize/repository/JdbcOfficerDaoTest.java
@@ -1,10 +1,14 @@
 package com.galvanize.repository;
 
+import com.galvanize.entities.Officer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,4 +47,15 @@ class JdbcOfficerDaoTest {
         //Teardown
     }
 
+    @Test
+    public void TestFindAllOfficers() {
+        //Setup
+        int expected = 5;
+        //Exercise
+        List<Officer> officerList = jdbcOfficerDao.findAllOfficers();
+        int actual = officerList.size();
+        //Assert
+        assertEquals(expected, actual);
+        //Teardown
+    }
 }
