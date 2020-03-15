3a4fb32528ecb83831a1e2b44a8b23b5f35dbb98
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-14 21:12:16 -0700

    added findAll
    all tests passed
    
    refactored rank variable name to officer_rank
    
    setup JpaOfficierDao.java with overriders
    
    create OfficerDao interface class
    
    create test for findAll

---
+++ src/main/java/com/galvanize/repository/JpaOfficerDao.java
@@ -0,0 +1,152 @@
+package com.galvanize.repository;
+
+import com.galvanize.entities.Officer;
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JpaOfficerDao implements OfficerDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public List<Officer> findAll() {
+        TypedQuery<Officer> query = entityManager.createQuery("select o from Officer o", Officer.class);
+        List<Officer> results = query.getResultList();
+        return results;
+    }
+
+    @Override
+    public List<Officer> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Officer> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Officer> findAllById(Iterable<Integer> integers) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0L;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(Officer entity) {
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Officer> entities) {
+    }
+
+    @Override
+    public void deleteAll() {
+    }
+
+    @Override
+    public <S extends Officer> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Officer> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Officer> findById(Integer integer) {
+        return null;
+    }
+
+    public Optional<Officer> findById2(long id) {
+        return null;
+    }
+
+    @Override
+    public boolean existsById(Integer integer) {
+        return false;
+    }
+
+    public boolean existsById(long id) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Officer> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<Officer> entities) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Officer getOne(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public <S extends Officer> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Officer> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Officer> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Officer> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Officer> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Officer> boolean exists(Example<S> example) {
+        return false;
+    }
+}
