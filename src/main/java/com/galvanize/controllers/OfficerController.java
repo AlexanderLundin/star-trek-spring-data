be3982fcd0833844b253f45ea274b77d16754352
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-28 20:25:52 -0700

    officers controller fully cruded

--- src/main/java/com/galvanize/controllers/OfficerController.java
+++ src/main/java/com/galvanize/controllers/OfficerController.java
@@ -1,92 +1,86 @@
-//package com.galvanize.controllers;
-//
-//import com.galvanize.entities.Officer;
-//import com.galvanize.entities.Rank;
-//import com.galvanize.repositories.JdbcOfficerDao;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.view.RedirectView;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//
-//@RestController
-//public class OfficerController {
-//
-//
-//    @Autowired
-//    JdbcTemplate jdbcTemplate;
-//    JdbcOfficerDao jdbcOfficerDao;
-//    //JpaOfficerDao jpaOfficerDao;
-//
-//
-//    public OfficerController(JdbcTemplate jdbcTemplate){
-//        jdbcOfficerDao = new JdbcOfficerDao(jdbcTemplate);
-//        //this.jpaOfficerDao = jpaOfficerDao;
-//    }
-//
-//
-//    //CREATE
-//
-//
-//    @PostMapping("/officers")
-//    public Officer JDBCPostOfficer(@RequestBody Officer officer){
-//        return jdbcOfficerDao.save(officer);
-//    }
-//
-//
-//    @RequestMapping(method = RequestMethod.POST, value = "/officers", params = {"first", "last", "rank"})
-//    public RedirectView officerAdd(@RequestParam String first,
-//                                @RequestParam String last, @RequestParam String rank, Model model) {
-//        Officer newOfficer = new Officer();
-//        newOfficer.setFirst(first);
-//        newOfficer.setLast(last);
-//        Rank doesRankExist = Rank.getIfPresent(rank);
-//        if (doesRankExist != null){
-//            newOfficer.setRank(Rank.valueOf(rank));
-//        }else{
-//            newOfficer.setRank(Rank.ENSIGN);
-//        }
-//        jdbcOfficerDao.save(newOfficer);
-//        model.addAttribute("officer", newOfficer);
-//        return new RedirectView("/officers/" + newOfficer.getId());
-//    }
-//
-//
-//    //READ
-//
-//
-//    @GetMapping("/officers")
-//    public List<Officer> JDBCGetAllOfficers() {
-//        List<Officer> officerList = jdbcOfficerDao.findAllOfficers();
-//        return officerList;
-//    }
-//
-//    @GetMapping("/officers/{id}")
-//    public Optional<Officer> JDBCGetOfficerByID(@PathVariable Long id){
-//        Optional<Officer> officer = jdbcOfficerDao.findOfficerById(id);
-//        return officer;
-//    }
-//
-//
-//    //UPDATE
-//
-//
-////    @PatchMapping("/officers/{id}/{rank}")
-////    public Officer JPAPatchOfficerRank(@PathVariable Long id, @PathVariable Rank rank){
-////        return jpaOfficerDao.updateRankByID(id, rank);
-////    }
-//
-//
-//    // DELETE
-//
-//
-//    @DeleteMapping("/officers/{id}")
-//    public void JBDCDeleteOfficerByID( @PathVariable Long id){
-//        jdbcOfficerDao.delete(id);
-//    }
-//
-//}
+package com.galvanize.controllers;
+
+import com.galvanize.entities.Officer;
+import com.galvanize.entities.Rank;
+import com.galvanize.repositories.JpaOfficerDao;
+import com.galvanize.services.OfficerService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+public class OfficerController {
+
+    OfficerService officerService;
+
+
+    public OfficerController(OfficerService officerService){
+        this.officerService = officerService;
+    }
+
+
+    //CREATE
+
+
+    @PostMapping("/officers")
+    public Officer JDBCPostOfficer(@RequestBody Officer officer){
+        return officerService.save(officer);
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "/officers", params = {"first", "last", "rank"})
+    public RedirectView officerAdd(@RequestParam String first,
+                                @RequestParam String last, @RequestParam String rank, Model model) {
+        Officer newOfficer = new Officer();
+        newOfficer.setFirst(first);
+        newOfficer.setLast(last);
+        Rank doesRankExist = Rank.getIfPresent(rank);
+        if (doesRankExist != null){
+            newOfficer.setRank(Rank.valueOf(rank));
+        }else{
+            newOfficer.setRank(Rank.ENSIGN);
+        }
+        officerService.save(newOfficer);
+        model.addAttribute("officer", newOfficer);
+        return new RedirectView("/officers/" + newOfficer.getId());
+    }
+
+
+    //READ
+
+
+    @GetMapping("/officers")
+    public List<Officer> JDBCGetAllOfficers() {
+        List<Officer> officerList = officerService.findAll();
+        return officerList;
+    }
+
+    @GetMapping("/officers/{id}")
+    public Officer JDBCGetOfficerByID(@PathVariable Long id){
+        Officer officer = officerService.findById(id);
+        return officer;
+    }
+
+
+    //UPDATE
+
+
+    @PatchMapping("/officers/{id}")
+    public Officer JPAPatchOfficerRank(@PathVariable Long id, @RequestBody Officer officer){
+        return officerService.updateById(id, officer);
+    }
+
+
+    // DELETE
+
+
+    @DeleteMapping("/officers/{id}")
+    public void JBDCDeleteOfficerByID( @PathVariable Long id){
+        officerService.deleteById(id);
+    }
+
+}
--- src/test/java/com/galvanize/controllers/OfficerControllerTest.java
+++ src/test/java/com/galvanize/controllers/OfficerControllerTest.java
@@ -1,138 +1,170 @@
-//package com.galvanize.controllers;
-//// rlw - unused imports (code/Optimize Imports)
-//import com.galvanize.entities.Rank;
-//import com.galvanize.repositories.JdbcOfficerDao;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//// rlw - Clean up your unused imports befor submitting (from the menu 'code/optimize imports')
-//
-//import javax.transaction.Transactional;
-//
-//import static org.hamcrest.Matchers.containsString;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@Transactional
-//// references from application-test.properties
-//@ActiveProfiles("test")
-//class OfficerControllerTest {
-//    // rlw - Watch your spacing on things like this.  Major elements of code should have a new line between them
-//    @Autowired
-//    MockMvc mvc;
-//    // rlw - should be a new empty line here
-//    // rlw - jdbcTemplate is never used - should not be here
-//    @Autowired
-//    JdbcTemplate jdbcTemplate;
-//    // rlw - new nine here
-//    // rlw -
-//    OfficerController controller;
-//
-//    // rlw - jpaOfficerDao is never used.  Should be removed
-//    //JpaOfficerDao jpaOfficerDao;
-//
-//    @BeforeEach
-//    public void Setup() {
-//        // rlw - jpaOfficerDao is never used.  Should be removed
-//        //Setup
-//        ///jpaOfficerDao = new JpaOfficerDao();
-//        // rlw - this is never used.  Also, wrong way to instantiate.  Should be autowired
-//        controller = new OfficerController(jdbcTemplate);
-//    }
-//
-//
-//    // CREATE
-//
-//
-//    @Test
-//    void TestPostOfficer() throws Exception {
-//        String body = "{\"id\":\"7\",\"rank\":\""+ Rank.ENSIGN +"\",\"first\":\"new\",\"last\":\"officer\"}";
-//        mvc.perform(post("/officers")
-//                .content(body)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON)
-//        )
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(jsonPath("$.first").value("new"));
-//    }
-//
-//
-//    //READ
-//
-//
-//    @Test
-//    public void TestGetAllOfficers() throws Exception {
-//        //Setup
-//        String url = "/officers";
-//        //Exercise and Assert
-//        mvc.perform(get(url))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(containsString("Kirk")));
-//        //Teardown
-//    }
-//
-//    @Test
-//    public void TestGetOfficerByID() throws Exception {
-//        //Setup
-//        String url = "/officers/2";
-//        //Exercise and Assert
-//        mvc.perform(get(url))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(containsString("Picard")));
-//        //Teardown
-//    }
-//
-//    @Test
-//    public void TestGetOfficerByIDInvalid() throws Exception {
-//        //Setup
-//        String url = "/officers/0";
-//        //Exercise and Assert
-//        mvc.perform(get(url))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//        //Teardown
-//    }
-//
-//
-//    //UPDATE
-//
-//
-//    @Test
-//    void TestPatchOfficerRank() throws Exception {
-//        //Setup
-//        String url = "/officers/1/"+ Rank.ENSIGN;
-//        //Exercise and Assert
-//        mvc.perform(patch(url))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(containsString(Rank.ENSIGN.toString())));
-//        //Teardown
-//    }
-//
-//
-//    //DELETE
-//
-//
-//    @Test
-//    public void TestDeleteOfficerByID() throws Exception {
-//        //Setup
-//        String url = "/officers/1/";
-//        //Exercise and Assert
-//        mvc.perform(delete(url))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//        //Teardown
-//    }
-//}
+package com.galvanize.controllers;
+// rlw - unused imports (code/Optimize Imports)
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.entities.Officer;
+import com.galvanize.entities.Rank;
+import com.galvanize.repositories.JpaOfficerDao;
+import com.galvanize.services.OfficerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+// references from application-test.properties
+@ActiveProfiles("test")
+class OfficerControllerTest {
+
+
+    @Autowired
+    MockMvc mvc;
+
+
+    @Autowired
+    OfficerService officerService;
+
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    private Officer officer1;
+    private String first = "James II";
+    private String last = "Kirk";
+    private Rank rank = Rank.COMMANDER;
+
+    @BeforeEach
+    public void setup() {
+        //Setup
+        officer1 = new Officer(rank, first, last);
+        officerService.save(officer1);
+    }
+
+
+    // CREATE
+
+
+    @Test
+    void TestPostOfficer() throws Exception {
+        String jsonInString = objectMapper.writeValueAsString(officer1);
+        ResultActions resultActions = mvc.perform(post("/officers")
+                .content(jsonInString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andDo(print());
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        Officer officer = objectMapper.readValue(contentAsString, Officer.class);
+        //Assert
+        assertEquals(Rank.ENSIGN, officer.getRank());
+        //Teardown
+    }
+
+
+    //READ
+
+
+    @Test
+    public void TestGetAllOfficers() throws Exception {
+        //Setup
+        String url = "/officers";
+        //Exercise and Assert
+        ResultActions resultActions = mvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk());
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        List<Officer> cars = Arrays.asList(objectMapper.readValue(contentAsString, Officer[].class));
+        int actual = cars.size();
+        int unexpected = 0;
+        //Assert
+        assertNotEquals(unexpected, actual);
+        //Teardown
+    }
+
+    @Test
+    public void TestGetOfficerByID() throws Exception {
+        //Setup
+        String url = "/officers/2";
+        //Exercise and Assert
+        mvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Picard")));
+        //Teardown
+    }
+
+    @Test
+    public void TestGetOfficerByIDInvalid() throws Exception {
+        //Setup
+        String url = "/officers/0";
+        //Exercise and Assert
+        mvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Teardown
+    }
+
+
+    //UPDATE
+
+
+    @Test
+    void TestPatchOfficer() throws Exception {
+        //Setup
+        String jsonInString = objectMapper.writeValueAsString(officer1);
+        String url = "/officers/1";
+        //Exercise and Assert
+        ResultActions resultActions = mvc.perform(patch(url)
+                .content(jsonInString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        Officer officer = objectMapper.readValue(contentAsString, Officer.class);
+        //Assert
+        assertEquals(Rank.COMMANDER, officer.getRank());
+        //Teardown
+    }
+
+
+    //DELETE
+
+
+    @Test
+    public void TestDeleteOfficerByID() throws Exception {
+        //Setup
+        String url = "/officers/1/";
+        //Exercise and Assert
+        mvc.perform(delete(url))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //Teardown
+    }
+}
